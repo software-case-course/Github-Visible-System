18bf4c2e3dae41268b909f9bcacc4a1108d7b17f
Telent1 <1418347552@qq.com>
2017-06-21 21:48:34 +0800

    修复代码错误

--- git-view-front/gitview1.4/src/store/index.js
+++ git-view-front/gitview1.4/src/store/index.js
@@ -3,7 +3,7 @@ import vuex from 'vuex'
 import * as actions from './actions'
 import * as getters from './getters'
 import showNav from './modules/showNav'
-import gettoken from './modules/gettoken'
+import gettoken from './modules/Gettoken'
 import permsg from './modules/Permsg'
 Vue.use(vuex)
 
